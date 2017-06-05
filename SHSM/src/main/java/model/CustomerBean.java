16533deb0ed99ebf51795705d1f61576e5dd3ef0
陳韋嘉 <eeit94@chen>
2017-06-05 10:27:10 +0800

    Chen Test 06/05

---
+++ SHSM/src/main/java/model/CustomerBean.java
@@ -0,0 +1,104 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@Entity
+@Table(name="customer")
+public class CustomerBean {
+	@Id
+	private String cus_id;
+	private String cus_password;
+	private String cus_name;
+	private String cus_address;
+	private String cus_phone;
+	private java.util.Date bdate;
+	private String cus_email;
+	private String sex;
+	
+//	public static void main(String[] args) {
+//		Configuration config = new Configuration().configure("hibernate.cfg.xml");
+//		SessionFactory sf = config.buildSessionFactory();
+//		try {
+//			sf.getCurrentSession().beginTransaction();
+//			Session session = sf.getCurrentSession();
+//			
+//			CustomerBean select = session.get(CustomerBean.class, "A");
+//			System.out.println("select=" + select);
+//			
+////			CustomerBean insert = new CustomerBean();
+////			insert.setCustid("Alex");
+////			insert.setPassword("xxx".getBytes());
+////			insert.setBirth(new java.util.Date());
+////			insert.setEmail("alex@iii.org.tw");
+////			session.save(insert);
+//			
+//			sf.getCurrentSession().getTransaction().commit();
+//			sf.getCurrentSession().close();
+//		} finally {
+//			sf.close();;
+//		}
+//	}
+	@Override
+	public String toString() {
+		
+		return "CustomerBean ["+ cus_id+", "+cus_password+", "+ cus_name + "]";
+	}
+	public String getCus_id() {
+		return cus_id;
+	}
+	
+	public void setCus_id(String cus_id) {
+		this.cus_id = cus_id;
+	}
+	public String getCus_password() {
+		return cus_password;
+	}
+	public void setCus_password(String cus_password) {
+		this.cus_password = cus_password;
+	}
+	public String getCus_name() {
+		return cus_name;
+	}
+	public void setCus_name(String cus_name) {
+		this.cus_name = cus_name;
+	}
+	public String getCus_address() {
+		return cus_address;
+	}
+	public void setCus_address(String cus_address) {
+		this.cus_address = cus_address;
+	}
+	public String getCus_phone() {
+		return cus_phone;
+	}
+	public void setCus_phone(String cus_phone) {
+		this.cus_phone = cus_phone;
+	}
+	public java.util.Date getBdate() {
+		return bdate;
+	}
+	public void setBdate(java.util.Date bdate) {
+		this.bdate = bdate;
+	}
+	public String getCus_email() {
+		return cus_email;
+	}
+	public void setCus_email(String cus_email) {
+		this.cus_email = cus_email;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
+
+}
---
+++ SHSM/src/main/java/model/CustomerService.java
@@ -0,0 +1,48 @@
+package model;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import model.dao.CustomerDAO;
+
+public class CustomerService {
+	private ICustomerDAO customerDao;
+	public CustomerService(ICustomerDAO customerDao) {
+		this.customerDao = customerDao;
+	}
+	
+	public CustomerBean login(String username, String password) {
+		CustomerBean bean = customerDao.select(username);
+		if(bean!=null) {
+			if(password!=null && password.length()!=0) {
+				String pass = password;	
+				String temp = bean.getCus_password();	
+				if(pass.equals(temp)) {
+					return bean;
+				}
+			}
+		}
+		return null;
+	}
+	
+//	public static void main(String[] args) {
+//		Configuration config = new Configuration().configure("hibernate.cfg.xml");
+//		SessionFactory sf = config.buildSessionFactory();
+//		try {
+//			sf.getCurrentSession().beginTransaction();
+//
+//			CustomerService customerService = new CustomerService(
+//					new CustomerDAO(sf.getCurrentSession()));
+//			
+//			CustomerBean bean = customerService.login("A", "a");
+//			System.out.println("bean="+bean);
+//			
+//			
+//		
+//			sf.getCurrentSession().getTransaction().commit();
+//			sf.getCurrentSession().close();
+//		} finally {
+//			sf.close();
+//		}
+//	}
+}
