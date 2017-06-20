0e8295be8364df31ca1d03fd61326af3d880aa6b
陳韋嘉 <eeit94@chen>
2017-06-20 14:30:36 +0800

    CHEMN

--- SHSM/src/main/java/controller/CaseDetailServlet.java
+++ SHSM/src/main/java/controller/CaseDetailServlet.java
@@ -48,6 +48,7 @@ public class CaseDetailServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("1231542132");
 		request.setCharacterEncoding("UTF-8");
 		//接收資料
 		String id = request.getParameter("id");
